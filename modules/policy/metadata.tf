b9c7ff845e049f4f8c4b56bbd25c31321e33c3c6
islamelkadi <ielkadi@icloud.com>
2026-03-07 23:25:39 -0500

    Pin metadata module to v1.0.0 in all submodules

--- modules/policy/metadata.tf
+++ modules/policy/metadata.tf
@@ -1,6 +1,6 @@
 # Metadata module for consistent naming and tagging
 module "metadata" {
-  source = "github.com/islamelkadi/terraform-aws-metadata"
+  source = "github.com/islamelkadi/terraform-aws-metadata?ref=v1.0.0"
 
   namespace     = var.namespace
   project_name  = var.name
